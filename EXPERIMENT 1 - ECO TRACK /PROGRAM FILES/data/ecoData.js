eeae5e47a8b0c22a3b20de99bcbe5db8a02002f5
Ayush Rana <162277875+Ayushrana27@users.noreply.github.com>
2026-01-18 15:27:01 +0530

    Delete EXPERIMENT 1 - ECO TRACK /PROGRAM FILES/data directory

--- EXPERIMENT 1 - ECO TRACK /PROGRAM FILES/data/ecoData.js
+++
@@ -1,7 +0,0 @@
-const ecoData = [
-  { id: 1, activity: "Tree Plantation", points: 100 },
-  { id: 2, activity: "Using Bicycle", points: 40 },
-  { id: 3, activity: "Waste Recycling", points: 60 }
-];
-
-export default ecoData;
